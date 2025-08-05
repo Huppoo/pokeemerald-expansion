c6363e5efc73af2f24c363c1d277c0368701dd55
Huppoo <jhupton1@hotmail.com>
2025-08-05 18:13:52 +1000

    Fixed Steven OW

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -349,7 +349,7 @@ $(OBJEVENTGFXDIR)/people/scott.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
 $(OBJEVENTGFXDIR)/people/steven.4bpp: %.4bpp: %.png
-	$(GFX) $< $@ -mwidth 4 -mheight 4
+	$(GFX) $< $@ -mwidth 2 -mheight 4
 
 $(OBJEVENTGFXDIR)/people/swimmer_f.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -629,15 +629,15 @@ const struct SpriteFrameImage gObjectEventPicTable_Dawn[] = {
 };
 
 const struct SpriteFrameImage gObjectEventPicTable_Steven[] = {
-    overworld_frame(gObjectEventPic_Steven, 4, 4, 0),
-    overworld_frame(gObjectEventPic_Steven, 4, 4, 1),
-    overworld_frame(gObjectEventPic_Steven, 4, 4, 2),
-    overworld_frame(gObjectEventPic_Steven, 4, 4, 3),
-    overworld_frame(gObjectEventPic_Steven, 4, 4, 4),
-    overworld_frame(gObjectEventPic_Steven, 4, 4, 5),
-    overworld_frame(gObjectEventPic_Steven, 4, 4, 6),
-    overworld_frame(gObjectEventPic_Steven, 4, 4, 7),
-    overworld_frame(gObjectEventPic_Steven, 4, 4, 8),
+    overworld_frame(gObjectEventPic_Steven, 2, 4, 0),
+    overworld_frame(gObjectEventPic_Steven, 2, 4, 1),
+    overworld_frame(gObjectEventPic_Steven, 2, 4, 2),
+    overworld_frame(gObjectEventPic_Steven, 2, 4, 3),
+    overworld_frame(gObjectEventPic_Steven, 2, 4, 4),
+    overworld_frame(gObjectEventPic_Steven, 2, 4, 5),
+    overworld_frame(gObjectEventPic_Steven, 2, 4, 6),
+    overworld_frame(gObjectEventPic_Steven, 2, 4, 7),
+    overworld_frame(gObjectEventPic_Steven, 2, 4, 8),
 };
 
 static const struct SpriteFrameImage sPicTable_Tate[] = {
